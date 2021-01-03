bd734d794b83ada773f964c8501d782f4a2d19c8
honoratoamg <69965835+honoratoamg@users.noreply.github.com>
2021-01-03 16:35:08 -0300

    Atualização Read-me

--- lib/app/utils/consts.dart
+++ lib/app/utils/consts.dart
@@ -1,6 +1,6 @@
 // Constantes de configuracoes em caixa alta
 
-/// Obs: 10.0.2.2 eh o ip padrao do emulador AVD.
+/// Obs: 10.0.2.2 eh o ip padrao do emulador AVD
 //const URL_BASE = 'http://localhost:3000/livros';
 const URL_BASE = 'http://10.0.2.2:3000/livros';
 //const URL_BASE = 'http://ip.do.seu.dispositivo.3000/livros';
