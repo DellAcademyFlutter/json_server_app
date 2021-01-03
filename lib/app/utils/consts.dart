cad386b4a62014b01c776bf7056c7f018b89936a
eliasciceros <69965844+eliasciceros@users.noreply.github.com>
2021-01-03 16:03:25 -0300

    Update consts.dart

--- lib/app/utils/consts.dart
+++ lib/app/utils/consts.dart
@@ -3,5 +3,5 @@
 /// Obs: 10.0.2.2 eh o ip padrao do emulador AVD.
 //const URL_BASE = 'http://localhost:3000/livros';
 const URL_BASE = 'http://10.0.2.2:3000/livros';
-//const URL_BASE = 'http://192.168.0.104:3000/livros';
+//const URL_BASE = 'http://Seu.Ip:3000/livros';
 
